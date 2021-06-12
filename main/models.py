d3bd76fc131fe84a449d43e65de8bb182472e7b6
adikzz <social.adik@gmail.com>
2021-06-12 20:40:33 +0600

    first commit

---
+++ main/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+class Users(models.Model):
+    username = models.CharField(max_length=255, unique=True)
+    full_name = models.CharField(max_length=500)
+    password = models.CharField(max_length=64)
+    updated_at = models.DateTimeField(verbose_name='updated_at', auto_now_add=True)
+    created_at = models.DateTimeField(verbose_name='created_at', auto_now=True)
